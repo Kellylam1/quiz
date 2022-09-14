ab21e68c941baec2a892c498844cfb768bc24e1c
Kellylam1 <112954444+Kellylam1@users.noreply.github.com>
2022-09-14 10:13:22 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 input.onButtonPressed(Button.A, function () {
-    count = count - 1
+    count = count * 2
 })
 input.onButtonPressed(Button.B, function () {
-    count = count / 1
+    count = count + 1
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(1)
 })
 let count = 0
-count = 0
-basic.forever(function () {
-    basic.showNumber(count)
-})
+count = 2
